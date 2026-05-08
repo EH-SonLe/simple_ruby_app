b3a551e826127661e5a756ccb578822573e35fb2
EH-SonLe <son.levan@employmenthero.com>
2026-05-08 11:25:53 +0700

    Create user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_05_05_045429) do
+ActiveRecord::Schema[7.2].define(version: 2026_05_08_041844) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,5 +19,12 @@ ActiveRecord::Schema[7.2].define(version: 2026_05_05_045429) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "email"
   end
 end
